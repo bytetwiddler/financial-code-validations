b7b7c0e5366acbf8b4ea803f2194b46a757f7175
twiddlebits <28405084+bytetwiddler@users.noreply.github.com>
2023-02-03 10:14:42 -0800

    Testing nested packages

---
+++ validators/aba/routing-numbers/go.mod
@@ -0,0 +1,3 @@
+module github.com/validators/aba/routing-numbers
+
+go 1.19
---
+++ validators/aba/routing-numbers/routing-numbers.go
@@ -0,0 +1,7 @@
+package routing-numbers
+
+import "fmt"
+
+func Helo(name string) string {
+	return fmt.Sprintf("Helo %v", name)
+}
